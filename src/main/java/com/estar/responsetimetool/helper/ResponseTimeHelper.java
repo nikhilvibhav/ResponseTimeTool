b8f60a6a2a38a2d6600b4905da5e8628b33964c1
Nikhil Vibhav <nikhil.vibhav@capgemini.com>
2015-06-10 16:42:54 +0530

    ResponseTimeTool.java has been separated into various methods for each journeys. Added HashMap to send data to the view class.

--- src/main/java/com/estar/responsetimetool/helper/ResponseTimeHelper.java
+++ src/main/java/com/estar/responsetimetool/helper/ResponseTimeHelper.java
@@ -1,6 +1,8 @@
 package com.estar.responsetimetool.helper;
 
 import com.estar.responsetimetool.pojo.TimeToLoad;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.commons.lang3.time.StopWatch;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,23 +17,69 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  */
 public class ResponseTimeHelper {
 
-    public void RTHelper() throws InterruptedException {
+    WebDriver driver;
+    StopWatch stopWatch;
+    WebElement element;
+    TimeToLoad timeToLoad;
+    HashMap pageLoadMap;
 
-        //Instantiate the RC Server
-        WebDriver driver = new FirefoxDriver();
-        
-        TimeToLoad timeToLoad = new TimeToLoad();
-                
-        StopWatch stopWatch = new StopWatch();
+    public ResponseTimeHelper() {
+
+        //Instantiate the selenium RC server
+        driver = new FirefoxDriver();
+        //Instantiate stopwatch
+        stopWatch = new StopWatch();
+        //Instantiate pageLoadTime map
+        pageLoadMap = new HashMap();
+
+    }
+
+    public Map initJourney() throws InterruptedException {
+        System.out.println("******************Init Journey******************");
+
+        timeToLoad = new TimeToLoad();
 
         //To visit Eurostar dot com
         driver.navigate().to("http://www.eurostar.com");
         stopWatch.start();
 
-        WebElement element = driver.findElement(By.linkText("United Kingdom"));
+        element = driver.findElement(By.linkText("United Kingdom"));
 
         stopWatch.stop();
-        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms");
+        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms\n");
+
+        timeToLoad.setPageTitle(driver.getTitle());
+        timeToLoad.setPageLoadTime(stopWatch.getTime());
+
+        pageLoadMap.put("Initial Journey", timeToLoad);
+
+        try {
+
+            homeJourney();
+            outboundJourney();
+            inboundJourney();
+            insuranceJourney();
+            userLoginJourney();
+            paxDetailsJourney();
+            paymentJourney();
+
+            driver.quit();
+
+            return pageLoadMap;
+        } catch (Exception E) {
+            System.out.println("Sorry the execution of the journey could not continue");
+            System.out.println(E.getMessage() + "\n");
+            
+            driver.quit();
+
+            return null;
+        }
+    }
+
+    public void homeJourney() throws InterruptedException {
+        System.out.println("******************Home Journey******************");
+
+        timeToLoad = new TimeToLoad();
 
         stopWatch.reset();
         element.click();
@@ -46,7 +94,18 @@ public class ResponseTimeHelper {
         });
 
         stopWatch.stop();
-        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms");
+        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms\n");
+
+        timeToLoad.setPageLoadTime(stopWatch.getTime());
+        timeToLoad.setPageTitle(driver.getTitle());
+
+        pageLoadMap.put("Homepage Journey", timeToLoad);
+    }
+
+    public void outboundJourney() throws InterruptedException {
+        System.out.println("******************Outbound Journey******************");
+
+        timeToLoad = new TimeToLoad();
 
         element = driver.findElement(By.xpath("//input[@value='Find trains' and @type='submit']"));
         stopWatch.reset();
@@ -70,10 +129,21 @@ public class ResponseTimeHelper {
         });
 
         stopWatch.stop();
-        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms");
+        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms\n");
 
-        element = driver.findElement(By.xpath("//*[@id='s" +
-                "tandard-booking-path-daily-view-form']/div/div[3]/table/tbody/tr[2]/td[5]/div"));
+        timeToLoad.setPageTitle(driver.getTitle());
+        timeToLoad.setPageLoadTime(stopWatch.getTime());
+
+        pageLoadMap.put("Outbound Journey", timeToLoad);
+    }
+
+    public void inboundJourney() throws InterruptedException {
+        System.out.println("******************Inbound Journey******************");
+
+        timeToLoad = new TimeToLoad();
+
+        element = driver.findElement(By.xpath("//*[@id='s"
+                + "tandard-booking-path-daily-view-form']/div/div[3]/table/tbody/tr[2]/td[5]/div"));
         stopWatch.reset();
         element.click();
 
@@ -98,11 +168,22 @@ public class ResponseTimeHelper {
         });
 
         stopWatch.stop();
+        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms\n");
 
-        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms");
+        timeToLoad.setPageTitle(driver.getTitle());
+        timeToLoad.setPageLoadTime(stopWatch.getTime());
 
-        element = driver.findElement(By.xpath("//*[@id='s" +
-                "tandard-booking-path-daily-view-form']/div/div[3]/table/tbody/tr[2]/td[5]/div"));
+        pageLoadMap.put("Inbound Journey", timeToLoad);
+
+    }
+
+    public void insuranceJourney() throws InterruptedException {
+        System.out.println("******************Insurance Journey******************");
+
+        timeToLoad = new TimeToLoad();
+
+        element = driver.findElement(By.xpath("//*[@id='s"
+                + "tandard-booking-path-daily-view-form']/div/div[3]/table/tbody/tr[2]/td[5]/div"));
         stopWatch.reset();
         element.click();
 
@@ -127,8 +208,18 @@ public class ResponseTimeHelper {
         });
 
         stopWatch.stop();
+        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms\n");
 
-        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms");
+        timeToLoad.setPageTitle(driver.getTitle());
+        timeToLoad.setPageLoadTime(stopWatch.getTime());
+
+        pageLoadMap.put("Insurance Journey", timeToLoad);
+    }
+
+    public void userLoginJourney() throws InterruptedException {
+        System.out.println("******************Login Journey******************");
+
+        timeToLoad = new TimeToLoad();
 
         stopWatch.reset();
         element = driver.findElement(By.xpath("//*[@id='dialog-element--2']"));
@@ -147,8 +238,18 @@ public class ResponseTimeHelper {
         });
 
         stopWatch.stop();
+        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms\n");
 
-        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms");
+        timeToLoad.setPageTitle(driver.getTitle());
+        timeToLoad.setPageLoadTime(stopWatch.getTime());
+
+        pageLoadMap.put("User Login Journey", timeToLoad);
+    }
+
+    public void paxDetailsJourney() throws InterruptedException {
+        System.out.println("******************Passenger Details Journey******************");
+
+        timeToLoad = new TimeToLoad();
 
         stopWatch.reset();
         element = driver.findElement(By.xpath("//*[@id='dialog-element']"));
@@ -164,8 +265,18 @@ public class ResponseTimeHelper {
         });
 
         stopWatch.stop();
+        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms\n");
 
-        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms");
+        timeToLoad.setPageTitle(driver.getTitle());
+        timeToLoad.setPageLoadTime(stopWatch.getTime());
+
+        pageLoadMap.put("PAX Details Journey", timeToLoad);
+    }
+
+    public void paymentJourney() throws InterruptedException {
+        System.out.println("******************Payment Journey******************");
+
+        timeToLoad = new TimeToLoad();
 
         stopWatch.reset();
         Select dropdown = new Select(driver.findElement(By.xpath("//*[@id='edit-adult-1-title']")));
@@ -188,10 +299,12 @@ public class ResponseTimeHelper {
         });
 
         stopWatch.stop();
+        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms\n");
 
-        System.out.println("Response Time to Page " + driver.getTitle() + " : " + stopWatch.getTime() + " ms");
+        timeToLoad.setPageTitle(driver.getTitle());
+        timeToLoad.setPageLoadTime(stopWatch.getTime());
 
-        stopWatch.reset();
-        driver.quit();
+        pageLoadMap.put("Payment Journey", timeToLoad);
+
     }
 }
--- src/main/java/com/estar/responsetimetool/view/ResponseTimeTool.java
+++ src/main/java/com/estar/responsetimetool/view/ResponseTimeTool.java
@@ -1,6 +1,8 @@
 package com.estar.responsetimetool.view;
 
 import com.estar.responsetimetool.helper.ResponseTimeHelper;
+import com.estar.responsetimetool.pojo.TimeToLoad;
+import java.util.Map;
 
 /**
  * Created by nvibhav on 09-Jun-15.
@@ -10,7 +12,42 @@ public class ResponseTimeTool {
     public static void main(String args[]) throws InterruptedException {
 
         ResponseTimeHelper responseTimeHelper = new ResponseTimeHelper();
-        responseTimeHelper.RTHelper();
+        Map map = responseTimeHelper.initJourney();
+
+        printLoadTimes(map);
+
+    }
+
+    public static void printLoadTimes(Map map) {
+        
+        TimeToLoad loadTimeInit;
+        TimeToLoad loadTimeHome;
+        TimeToLoad loadTimeOutbound;
+        TimeToLoad loadTimeInbound;
+        TimeToLoad loadTimeInsurance;
+        TimeToLoad loadTimeLogin;
+        TimeToLoad loadTimePaxDetails;
+        TimeToLoad loadTimePayment;
+
+        loadTimeInit = (TimeToLoad) map.get("Initial Journey");
+        loadTimeHome = (TimeToLoad) map.get("Homepage Journey");
+        loadTimeOutbound = (TimeToLoad) map.get("Outbound Journey");
+        loadTimeInbound = (TimeToLoad) map.get("Inbound Journey");
+        loadTimeInsurance = (TimeToLoad) map.get("Insurance Journey");
+        loadTimeLogin = (TimeToLoad) map.get("User Login Journey");
+        loadTimePaxDetails = (TimeToLoad) map.get("PAX Details Journey");
+        loadTimePayment = (TimeToLoad) map.get("Payment Journey");
+
+        System.out.println("\n\n\n*********************FROM MAIN METHOD*********************\n\n\n");
+
+        System.out.println(loadTimeInit.getPageTitle() + " " + loadTimeInit.getPageLoadTime());
+        System.out.println(loadTimeHome.getPageTitle() + " " + loadTimeHome.getPageLoadTime());
+        System.out.println(loadTimeOutbound.getPageTitle() + " " + loadTimeOutbound.getPageLoadTime());
+        System.out.println(loadTimeInbound.getPageTitle() + " " + loadTimeInbound.getPageLoadTime());
+        System.out.println(loadTimeInsurance.getPageTitle() + " " + loadTimeInsurance.getPageLoadTime());
+        System.out.println(loadTimeLogin.getPageTitle() + " " + loadTimeLogin.getPageLoadTime());
+        System.out.println(loadTimePaxDetails.getPageTitle() + " " + loadTimePaxDetails.getPageLoadTime());
+        System.out.println(loadTimePayment.getPageTitle() + " " + loadTimePayment.getPageLoadTime());
 
     }
 }
