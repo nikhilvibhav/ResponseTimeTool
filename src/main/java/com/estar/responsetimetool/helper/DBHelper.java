2621a77aecef700796ae55475dfceaedc92a3a1a
Nikhil Vibhav <nikhil.vibhav@capgemini.com>
2015-07-30 16:09:47 +0530

    Added DBHelpler class. Empty for now

---
+++ src/main/java/com/estar/responsetimetool/helper/DBHelper.java
@@ -0,0 +1,16 @@
+package com.estar.responsetimetool.helper;
+
+import org.hibernate.Session;
+
+/**
+  *
+  * Created by nvibhav on 30 July
+  *
+  */
+public class DBHelper {
+
+  DBHelper() {
+      Session session = factory.openSession();
+  }
+
+}
