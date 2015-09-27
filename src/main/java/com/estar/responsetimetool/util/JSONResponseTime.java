b5f22dceae8ed4daaac8a0b54125da25d00a6d27
Nikhil Vibhav <nikhil92ind@gmail.com>
2015-09-28 00:07:49 +0530

    Added web service capability. Lets see if it works. Fingers crossed.

---
+++ src/main/java/com/estar/responsetimetool/util/JSONResponseTime.java
@@ -0,0 +1,10 @@
+package com.estar.responsetimetool.util;
+
+import org.json.simple.JSONObject;
+
+/**
+ * Created by Nikhil on 27-09-2015.
+ */
+public class JSONResponseTime extends JSONObject {
+
+}
